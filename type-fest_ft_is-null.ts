f344cf167ce4381b6d512b7f7b94d63ff8d7b7ba
zexigong <zexi.gong721@gmail.com>
2025-04-13 23:51:26 -0700

    Revert "update"
    This reverts commit 1bc97fc8a412cb13b90758affde987ab32e8dca2.

--- type-fest_ft_is-null.ts
+++
@@ -1,8 +0,0 @@
-import type {IsNull} from '../source/is-null';
-import {expectType} from 'tsd';
-
-declare const isNull: IsNull<null>;
-expectType<true>(isNull);
-
-declare const isNotNull: IsNull<number>;
-expectType<false>(isNotNull);
